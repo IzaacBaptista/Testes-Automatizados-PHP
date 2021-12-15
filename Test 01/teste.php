62e40e509d05a27fe17a254f4033522b3d8b2ecd
Izaac Baptista <izaacbaptista@gmail.com>
2021-12-15 16:07:30 -0300

    Testes Automatizados PHP

--- Test 01/teste.php
+++ Test 01/teste.php
@@ -3,18 +3,18 @@
 require_once 'EscritorDeArquivo.php';
 
 //criando ambiente
-$caminhoArquivo = 'arquivo';
+$caminhoArquivo = md5(time());
 $escritor = new EscritorDeArquivo($caminhoArquivo);
 
 //executando
 $escritor->escreve(conteudo: 'Primeira linha');
 $escritor->escreve(conteudo: 'Outra linha');
 
+//verificando
 $conteudoEsperado = 'Primeira linha
 Outra linha
 ';
 
-//verificando
 if ($conteudoEsperado === file_get_contents($caminhoArquivo)) {
     echo 'TESTE OK' . PHP_EQL;
 } else {
