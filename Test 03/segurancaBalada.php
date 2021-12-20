e6e450530c7890a0cc32ee5b271e9d3e02389e9b
Izaac Baptista <izaacbaptista@gmail.com>
2021-12-20 15:55:40 -0300

    TDD - Test-Driven Development

---
+++ Test 03/segurancaBalada.php
@@ -0,0 +1,14 @@
+<?php
+
+class SegurancaBalada
+{
+    public function deixaEntrar(Pessoa $pessoa):bool 
+    {
+        return $pessoa->idade() >= 18;
+    }
+
+    
+
+}
+
+?>
--- Test 03/teste-balada.php
+++ Test 03/teste-balada.php
@@ -1,18 +1,21 @@
 <?php
 
-require_once 'Pessoa.php';
+    require_once 'Pessoa.php';
+    require_once 'SegurancaBalada.php';
 
-//Arrange
-$pessoa = new Pessoa( nome: 'Izaac', new DateTimeImmutable(time: '1993-01-03'));
-$seguranca = new segurancaBalada();
+    //Arrange
+    $pessoa = new Pessoa( nome: 'Izaac', new DateTimeImmutable(time: '1993-01-03'));
+    $seguranca = new segurancaBalada();
 
-//Act
-$deixouPessoaEntrar = $seguranca->deixaEntrar($pessoa);
+    //Act
+    $deixouPessoaEntrar = $seguranca->deixaEntrar($pessoa);
 
 
-//Assert
-if($deixouPessoaEntrar === true) {
-    echo 'TESTE OK' . PHP_EQL;
-} else {
-    echo 'TESTE FALHOU' . PHP_EQL;
-}
+    //Assert
+    if($deixouPessoaEntrar === true) {
+        echo 'TESTE OK' . PHP_EQL;
+    } else {
+        echo 'TESTE FALHOU' . PHP_EQL;
+    }
+
+?>
